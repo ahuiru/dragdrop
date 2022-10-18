2772fbc448a4eab33b29ff4d939a9a0aca092f9d
ahuiru <zhuangerlangyou@gmail.com>
2022-10-18 23:15:51 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState(["item0", "item1", "item2"]);
+
+  const onDragEnd = (result) => {
+    console.log(result)
+    // console.log(results.source.index)
+    // console.log(results.destination.index)
+
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0])
+  };
+
+  return (
+    <div className="dragContainer">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item0" index={0}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[0]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default App;
